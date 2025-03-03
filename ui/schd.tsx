46179a79da9ed2f40869277e874d679ebcbe4d79
blalab <rcidfer@gmail.com>
2025-03-02 19:04:30 -0500

    removed useLocation from tool

--- ui/schd.tsx
+++ ui/schd.tsx
@@ -1,23 +1,28 @@
 //import ToolGarticCRUD from "@/tools/gartic/pages/tool_gartic_crud"
 import ToolDataCRUD from "../../../tools/data/ui/pages/tool_data_crud"
 
-import { useLocation } from "react-router-dom";
+//import { useLocation } from "react-router-dom";
 
-export default function Schd() {
+export default function Schd({ portfolio, org, tool, ring }: {
+  portfolio: string;
+  org: string;
+  tool: string;
+  ring?: string;  // optional prop since it might be undefined
+}) {
 
-    const location = useLocation();
-    const page = location.pathname.split('/')[4]
+    //const location = useLocation();
+    //const page = location.pathname.split('/')[4]
 
-    console.log('Page:',page)
+    console.log('Schd > Ring:',ring)
 
     return (
         <div className="flex min-h-screen w-full flex-col bg-muted/40">
         
           <div className="flex flex-col sm:gap-2 sm:pl-2">
 
-            {page === 'schd_jobs' && <ToolDataCRUD readonly={false} />}
-            {page === 'schd_runs' && <ToolDataCRUD readonly={true} />}
-            {page === 'schd_rules' && <ToolDataCRUD readonly={false} />}
+            {ring === 'schd_jobs' && <ToolDataCRUD readonly={false} portfolio={portfolio} org={org} tool={tool} ring={ring} />}
+            {ring === 'schd_runs' && <ToolDataCRUD readonly={true} portfolio={portfolio} org={org} tool={tool} ring={ring} />}
+            {ring === 'schd_rules' && <ToolDataCRUD readonly={false} portfolio={portfolio} org={org} tool={tool} ring={ring} />}
 
           </div>
         </div>
